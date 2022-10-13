0e16dc0de8dc2ecd8bc0a7994c5cad59a6da8cd6
sophi <sophi@Sophie.dhcp.wustl.edu>
2022-10-13 12:06:33 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,31 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+
 		
+	StdDraw.setXscale(0, 22);
+	StdDraw.setYscale(0, 16);
+
+		   
+	StdDraw.setPenColor(37,111,115);
+	StdDraw.filledRectangle(11, 8, 9, 5);
+	
+	StdDraw.setPenColor(Color.CYAN); 
+	StdDraw.filledCircle(11, 8, 3);
+		   
+	StdDraw.setPenColor(36,48,97); 
+	StdDraw.setPenRadius(.01);
+	StdDraw.rectangle(11, 8, 9, 5);
+	
+	StdDraw.setPenColor(Color.GREEN);
+	StdDraw.filledCircle(9.5, 8, 1);
+	StdDraw.filledCircle(12.5, 8, 1);  
+	
+	StdDraw.setPenColor(Color.DARK_GRAY); 
+	StdDraw.setPenRadius(.01);
+	StdDraw.rectangle(11, 6, 1.5, 0.0);
+		   
+		 
 	}
 }
